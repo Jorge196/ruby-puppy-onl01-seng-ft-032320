969697db85372e731d8e721687b62cba27f3efaa
Jorge Ramirez <joramirez96@gmail.com>
2020-04-03 18:42:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,11 +10,16 @@ class Dog
     @@all << self
   end
 
-  def self.print_all
+  def self.all
     @@all.each do |dog|
       puts dog.name
     end
   end
+    def self.print_all
+      @@all.each do |dog|
+        puts dog.name
+      end 
+    end
 
 
   def self.clear_all
